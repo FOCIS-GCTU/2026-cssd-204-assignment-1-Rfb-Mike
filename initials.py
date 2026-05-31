b5a4c481918f0f2c94d666fbe2941c0a1ee05176
Micheal Ampadu-Nyarko <nanyaws254@gmail.com>
2026-05-31 17:23:06 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,37 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: MICHEAL AMPADU-NYARKO
+# STUDENT ID: 2425403398
+# Email: 2425403398@live.gctu.edu.gh
+# Grader: AUGUSTUS BUCKMAN
+#
+# On my honor, MICHEAL AMPADU-NYARKO, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
 
 def main():
-  pass #code goes here
+    # Prints my initials in large letters
 
+    print()
+    print("...MAN")
+    print()
+
+    # Large initials go here
+    print("...MM..........MM............AA..............N........N......")
+    print("...M.M........M.M...........A..A.............NN.......N......")
+    print("...M..M......M..M..........A....A............N.N......N......")
+    print("...M...M....M...M.........A......A...........N..N.....N......")
+    print("...M....M..M....M........AAAAAAAAAA..........N...N....N......")
+    print("...M.....MM.....M........A........A..........N....N...N......")
+    print("...M............M........A........A..........N.....N..N......")
+    print("...M............M........A........A..........N......N.N......")
+    print("...M............M...**...A........A...**.....N.......NN....**")
+    print("...M............M...**...A........A...**.....N........N....**")
+
+    print()
 
 
 main()
